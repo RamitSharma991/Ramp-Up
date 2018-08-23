d6e2b5099c55789ca5e19a0e9f0ca43d34e9135b
RamitSharma991 <RamitsMac@ramits-mbp.fritz.box>
2018-08-23 10:23:57 +0530

    Placing Ramps in ARKit Live

--- Ramp-Up/Controller/RampPlacerVC.swift
+++ Ramp-Up/Controller/RampPlacerVC.swift
@@ -13,6 +13,7 @@ import ARKit
 class RampPlacerVC: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    var selectedRamp: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +25,8 @@ class RampPlacerVC: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/pipe.dae")!
+        let scene = SCNScene(named: "art.scnassets/main.scn")!
+        sceneView.autoenablesDefaultLighting = true
         
         // Set the scene to the view
         sceneView.scene = scene
@@ -78,6 +80,19 @@ class RampPlacerVC: UIViewController, ARSCNViewDelegate {
         
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {return}
+        
+        let results = sceneView.hitTest(touch.location(in: sceneView), types: [.featurePoint])
+        guard let hitFeature = results.last else { return }
+        let hitTransform = SCNMatrix4(hitFeature.worldTransform)
+        let hitPosition = SCNVector3Make(hitTransform.m41, hitTransform.m42, hitTransform.m43)
+        placeRamp(position: hitPosition)
+        
+        
+        
+    }
+    
     func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
         return .none
     }
@@ -94,7 +109,21 @@ class RampPlacerVC: UIViewController, ARSCNViewDelegate {
         
     }
     
-    func onRampSlected(_ rampname: String) {
+    func onRampSlected(_ rampName: String) {
         //
+        selectedRamp = rampName
     }
+    
+    func placeRamp(position: SCNVector3) {
+        if let rampName = selectedRamp {
+        let ramp = Ramp.getRampForName(rampName: rampName)
+            ramp.position = position
+            ramp.scale = SCNVector3Make(0.01, 0.01, 0.01)
+            sceneView.scene.rootNode.addChildNode(ramp)
+        
+    }
+        
+    }
+    
+    
 }
